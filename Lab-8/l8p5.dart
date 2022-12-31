315459a70ab3b2bd1625968ec63fab5f7411c48e
Kevin_Dhaduk <95095533+DhadukKevin@users.noreply.github.com>
2022-12-31 16:48:46 +0530

    Update l8p5.dart

--- Lab-8/l8p5.dart
+++ Lab-8/l8p5.dart
@@ -12,83 +12,53 @@ class _L8p5State extends State<L8p5> {
 
   @override
   Widget build(BuildContext context) {
-      return SafeArea(
-          child: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10),
-              child: TextField(
-                controller: myController,
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.blueAccent,
-                    ),
-                    borderRadius: BorderRadius.circular(10.0),
+     return SafeArea(
+        child: Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Center(
+              child: Text(txt),
+            ),
+            height: 100,
+            width: 500,
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: TextField(
+              controller: myController,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent,
                   ),
-                  hintText: "Enter Your Text...",
-                  hintStyle: TextStyle(
-                    color: Colors.purple,
-                    fontStyle: FontStyle.italic,
-                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                hintText: "Enter Your Text...",
+                hintStyle: TextStyle(
+                  color: Colors.purple,
+                  fontStyle: FontStyle.italic,
                 ),
               ),
             ),
-            Container(
-              color: Colors.green,
-              child: TextButton(
-                onPressed: () {
-                  // print('submit called');
-                  print(myController.text);
-                  // Navigator.pop(context,'heloo');
-
-                },
-                child: Text(
-                  'Submit',
-                  style: TextStyle(color: Colors.white),
-                ),
+          ),
+          Container(
+            color: Colors.green,
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  txt = myController.text;
+                });
+                print(myController.text);
+              },
+              child: Text(
+                'Submit',
+                style: TextStyle(color: Colors.white),
               ),
             ),
-            // Container(
-            //   margin: EdgeInsets.all(20),
-            //   height: 100,
-            //   width: 300,
-            //   // color: Colors.red,
-            // )
-          ],
-        ),
-      ));
-    // int _i = 0;
-    // return Scaffold(
-    //     appBar: AppBar(),
-    //     body: Column(
-    //       children: [
-    //         TextButton(
-    //           style: ButtonStyle(
-    //             foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-    //           ),
-    //           onPressed: () {
-    //             setState(() {
-    //               _i++;
-    //               print(_i);
-    //               print("Fidase");
-    //             });
-    //           },
-    //           child: Text('TextButton'),
-    //         ),
-    //         ListView.builder(
-    //             shrinkWrap: true,
-    //             itemCount: _i,
-    //             itemBuilder: (BuildContext ctxt, int index) {
-    //               return Container(
-    //                   color: Colors.blueGrey,
-    //                   height: 100,
-    //                   width: 100,
-    //                   child: Text("Container numero $_i"));
-    //             })
-    //       ],
-    //     )
-    // );
+          ),
+        ],
+      ),
+    ));
   }
 }
